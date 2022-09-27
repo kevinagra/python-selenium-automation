ffbde6b16fa0f6aec4b08abb19ebe2a86c017f3d
Kevin <qa.kevinagra@gmail.com>
2022-09-26 17:06:49 -0700

    Update product search test

---
+++ HW2_locators_practice.py
@@ -0,0 +1,20 @@
+"""
+HW2: Locating elements, strategy
+
+Amazon logo - search by class "a-icon a-icon-logo", and role "img"
+
+'Continue' button - search by type "email", and id "ap_email"
+
+'Need help?' link - search by class "a-expander-prompt"
+
+'Forgot your password?' link - search by id "auth-fpp-link-bottom"
+
+'Other issues with Sign-In' link - search by id "ap-other-signin-issues-link"
+
+'Create your Amazon account' button - search by id "createAccountSubmit"
+
+'Conditions of Use' link - search by class "a-link-normal", and href "/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088"
+
+'Privacy Notice' link - search by class "a-link-normal", and href "/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496"
+
+"""
