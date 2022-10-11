e60de850b6bdd7b487cd0087449077c4eab2e766
Kevin <qa.kevinagra@gmail.com>
2022-10-10 19:06:19 -0700

    Update product search test

--- features/steps/HW3_product_page_steps.py
+++ features/steps/HW3_product_page_steps.py
@@ -1,6 +1,16 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 
+
+COLOR_OPTIONS = (By.CSS_SELECTOR, "#variation_color_name li")
+CURRENT_COLOR = (By.CSS_SELECTOR, "#variation_color_name .selection")
+
+
+@given('Open Amazon product {product_id} page')
+def open_amazon_product(context, product_id):
+    context.driver.get(f'https://www.amazon.com/dp/{product_id}/')
+
+
 @when('Add to Cart')
 def add_to_cart(context):
     context.driver.find_element(By.CSS_SELECTOR, '#add-to-cart-button').click()
@@ -15,3 +25,16 @@ def add_to_cart(context):
 def add_to_cart(context):
     time.sleep(4)
     context.driver.find_element(By.CSS_SELECTOR, '#.a-button-input').click()
+
+@then('Verify user can click through colors')
+def verify_can_click_colors(context):
+    expected_colors = ['Army Green', 'Black', 'Blue']
+    actual_colors = []
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
+    for color in colors[:3]:
+        color.click()
+        current_color = context.driver.find_element(*CURRENT_COLOR).text
+        actual_colors += [current_color]
+    assert expected_colors == actual_colors, \
+        f' Expected colors {expected_colors} did not match actual {actual_colors}'
+
---
+++ features/tests/HW5_loop.feature
@@ -0,0 +1,9 @@
+# Created by Kevin at 10/10/22
+Feature: Loop test case
+  # Loop through colors: it should click on each color and verify that the color has been selected
+
+
+  Scenario: User can select colors
+    Given Open Amazon product B081YS2F7N page
+    Then Verify user can click through colors
+
